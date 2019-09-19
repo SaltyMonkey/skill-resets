cceae0cd5e14676c7066ec5f34f8b3ba681611cb
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2019-09-19 12:47:17 +0300

    prevent resets in bulk to show

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const iconsData = require('./iconsList.json');
 
 module.exports = function SkillResets(mod) {
+    let lastReset = { time: null, icon: null };
 
     function getSkillBase(skill) {
         return Math.floor(skill / 10000);
@@ -9,11 +10,14 @@ module.exports = function SkillResets(mod) {
     mod.hook('S_CREST_MESSAGE', 2, ({ type, skill }) => {
         if (type === 6) {
             let icon = iconsData[mod.game.me.class][skill] || iconsData[mod.game.me.class][getSkillBase(skill)];
-            mod.send('S_CUSTOM_STYLE_SYSTEM_MESSAGE', 1, {
-                message: `<img src="img://__${icon}" width="48" height="48" vspace="-20"/><font size="24" color="${mod.settings.reset_font_color}">&nbsp;Reset</font>`,
-                style: mod.settings.resetStyle
-            });
-
+            if (lastReset.icon !== icon || (lastReset.icon === icon && (Date.now() - lastReset.time > 15))) {
+                lastReset.icon = icon;
+                lastReset.time = Date.now();
+                mod.send('S_CUSTOM_STYLE_SYSTEM_MESSAGE', 1, {
+                    message: `<img src="img://__${icon}" width="48" height="48" vspace="-20"/><font size="24" color="${mod.settings.reset_font_color}">&nbsp;Reset</font>`,
+                    style: mod.settings.resetStyle
+                });
+            }
             if (mod.settings.sound)
                 mod.send('S_PLAY_SOUND', 1, { SoundID: mod.settings.soundId });
 
